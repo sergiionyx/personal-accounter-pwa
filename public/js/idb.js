133a5774e507c087916d24fffcf956d9ab3181dd
sergii-onyx <twilight.cympak@gmail.com>
2022-06-28 13:31:32 -0400

    added idb and manifest

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,6 +1,6 @@
 let db;
-// establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
-const request = indexedDB.open("budget_tracker", 1);
+// establish a connection to IndexedDB database called 'tracker' and set it to version 1
+const request = indexedDB.open("tracker", 1);
 
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
